f92a66c8cb628a9d0d80289b7644d636adba496a
Jeyavelrajan Selvaraj <jeyavelrajan.s@itp.objectfrontier.com>
2024-01-04 17:36:21 +0530

    first commit

---
+++ src/main/java/com/dms/Sb1DmsApplication.java
@@ -0,0 +1,13 @@
+package com.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1DmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1DmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dms/bean/Doctor.java
@@ -0,0 +1,66 @@
+package com.dms.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_doctor")
+public class Doctor {
+	@Id
+	@Column(name="dId")
+	private int dId;
+	@Column(name="dName")
+	private String dName;
+	@Column(name="dDept")
+	private String dDept;
+	@Column(name="dSalary")
+	private int dSalary;
+	public Doctor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Doctor(int dId, String dName, String dDept, int dSalary) {
+		super();
+		this.dId = dId;
+		this.dName = dName;
+		this.dDept = dDept;
+		this.dSalary = dSalary;
+	}
+	public int getdId() {
+		return dId;
+	}
+	public void setdId(int dId) {
+		this.dId = dId;
+	}
+	public String getdName() {
+		return dName;
+	}
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+	public String getdDept() {
+		return dDept;
+	}
+	public void setdDept(String dDept) {
+		this.dDept = dDept;
+	}
+	public int getdSalary() {
+		return dSalary;
+	}
+	public void setdSalary(int dSalary) {
+		this.dSalary = dSalary;
+	}
+	@Override
+	public String toString() {
+		return "Doctor [dId=" + dId + ", dName=" + dName + ", dDept=" + dDept + ", dSalary=" + dSalary + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
